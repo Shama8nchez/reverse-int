b4b4798e8df1598d97c0ee0f3c8177d137d0cbb9
Shama8nchez <shamanouski@gmail.com>
2022-01-09 13:43:14 +0300

    feet: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    n = -n;
+  }
+  let str = n.toString();
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    newStr = newStr + str[str.length - 1 - i];
+  }
+  return +newStr;
 }
